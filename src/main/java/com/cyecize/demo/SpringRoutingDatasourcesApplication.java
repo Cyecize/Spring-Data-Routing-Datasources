3f6f752dd49bdec9b45a1ef7f4d6965db15febae
Tsvetelin Tyakimov <ceci2205@abv.bg>
2022-01-26 20:32:36 +0200

    Create new project. Import MySql, MS SQL Server and Spring data.

---
+++ src/main/java/com/cyecize/demo/SpringRoutingDatasourcesApplication.java
@@ -0,0 +1,13 @@
+package com.cyecize.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRoutingDatasourcesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRoutingDatasourcesApplication.class, args);
+    }
+
+}
