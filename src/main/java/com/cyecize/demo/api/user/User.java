7edaedafcd0dd5f2703603261ad5f88597b19459
Tsvetelin Tyakimov <ceci2205@abv.bg>
2022-01-26 21:02:19 +0200

    Add skeleton

---
+++ src/main/java/com/cyecize/demo/api/user/User.java
@@ -0,0 +1,25 @@
+package com.cyecize.demo.api.user;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+}
---
+++ src/main/java/com/cyecize/demo/config/db/DataSourceConfig.java
@@ -0,0 +1,7 @@
+package com.cyecize.demo.config.db;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfig {
+}
